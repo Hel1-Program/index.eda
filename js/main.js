3e61cfefd1a5027b2874ee51092f91e11f3d7069
Hel1-Program <74363094+Hel1-Program@users.noreply.github.com>
2021-10-25 22:38:06 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+    
+    // Navigation arrows
+    navigation: {
+      nextEl: '.slyder-button-next',
+      prevEl: '.slyder-button-prev',
+    },
+  
+  });
